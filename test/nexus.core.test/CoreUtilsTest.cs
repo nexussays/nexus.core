aa0e74deecbd43ee386d76193f719e35a55ae615
Malachi Griffie <malachi@nexussays.com>
2016-12-03 05:11:54 -0800

    added tests for CoreUtils

---
+++ test/nexus.core.test/CoreUtilsTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace nexus.core.test
+{
+   [TestFixture]
+   internal class CoreUtilsTest
+   {
+      [Test]
+      public void utility_throw_method_works()
+      {
+         Assert.Throws<Exception>( CoreUtils.ThrowException );
+      }
+
+      [Test]
+      public void utilitydivide_by_0_method_works()
+      {
+         Assert.Throws<DivideByZeroException>( () => CoreUtils.ThrowDivideByZero() );
+      }
+   }
+}
