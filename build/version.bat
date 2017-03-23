15956e66497b960260c61aec816459d0d3508fe3
Malachi Griffie <malachi@nexussays.com>
2017-03-22 21:24:09 -0700

    updated version.bat build script

--- build/version.bat
+++ build/version.bat
@@ -2,11 +2,10 @@
 
 set VERSION={"build-id":%BUILD_BUILDID%, "version":"%GITVERSION_SEMVER%", "branch":"%GITVERSION_BRANCHNAME%", "build-num":"%BUILD_BUILDNUMBER%", "user":"%BUILD_REQUESTEDFOR% <%BUILD_REQUESTEDFOREMAIL%>"}
 
+echo %VERSION%
+echo %VERSION% > version.json
+
 if %GITVERSION_BRANCHNAME% == master (
-   echo %VERSION% > version.json
-   echo %VERSION%
    git tag -a "%GITVERSION_SEMVER%" -F version.json
    git push --progress origin tag %GITVERSION_SEMVER%
-) else (
-   echo %VERSION%
 )
