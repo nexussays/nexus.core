dc3a0eee5ca2ac4858eac01705bc214e98ff331e
Malachi Griffie <malachi@nexussays.com>
2017-04-10 02:20:18 -0700

    added some utility methods and bunch fo doc comments to Option

--- src/nexus.core/Option.cs
+++ src/nexus.core/Option.cs
@@ -18,6 +18,9 @@ namespace nexus.core
       : IEquatable<Option<T>>,
         IEquatable<T>
    {
+      /// <summary>
+      ///    <c>default(Option&lt;T&gt;)</c>
+      /// </summary>
       public static readonly Option<T> NoValue = default(Option<T>);
 
       private readonly T m_value;
@@ -29,11 +32,16 @@ namespace nexus.core
       }
 
       /// <summary>
-      /// The value of this option, if available. Ensure you check <see cref="HasValue" /> before accessing this property.
+      /// <c>true</c> if this option type contains a value.
       /// </summary>
-      /// <exception cref="InvalidOperationException">If access is attempted when <see cref="HasValue" /> is false.</exception>
       public Boolean HasValue { get; }
 
+      /// <summary>
+      /// The resolved value of this option type, or throws <see cref="InvalidOperationException" /> if <see cref="HasValue" />
+      /// is
+      /// <c>false</c>
+      /// </summary>
+      /// <exception cref="InvalidOperationException">If access is attempted when <see cref="HasValue" /> is false.</exception>
       public T Value
       {
          get
@@ -47,6 +55,7 @@ namespace nexus.core
          }
       }
 
+      /// <inheritdoc />
       [Pure]
       public override Boolean Equals( Object obj )
       {
@@ -57,28 +66,35 @@ namespace nexus.core
          return HasValue && m_value.Equals( obj );
       }
 
+      /// <inheritdoc />
       [Pure]
       public Boolean Equals( T other )
       {
          return HasValue && m_value.Equals( other );
       }
 
+      /// <inheritdoc />
       [Pure]
       public Boolean Equals( Option<T> other )
       {
-         return (other.HasValue == HasValue) && (!HasValue || Equals( m_value, other.m_value ));
+         return other.HasValue == HasValue && (!HasValue || Equals( m_value, other.m_value ));
       }
 
+      /// <inheritdoc />
       public override Int32 GetHashCode()
       {
          return HasValue ? (ReferenceEquals( m_value, null ) ? -1 : m_value.GetHashCode()) : 0;
       }
 
+      /// <inheritdoc />
       public override String ToString()
       {
-         return HasValue ? (ReferenceEquals( m_value, null ) ? "null" : m_value.ToString()) : "No Value";
+         return HasValue ? (ReferenceEquals( m_value, null ) ? "null" : m_value.ToString()) : "[undefined]";
       }
 
+      /// <summary>
+      /// Implicitly wrap any <typeparamref name="T" /> to an <see cref="Option{T}" />
+      /// </summary>
       public static implicit operator Option<T>( T value )
       {
          return new Option<T>( value );
@@ -87,23 +103,71 @@ namespace nexus.core
 
    public static class Option
    {
+      /// <summary>
+      ///    <c>default(Option&lt;Object&gt;)</c>
+      /// </summary>
       public static readonly Option<Object> NoValue = default(Option<Object>);
 
+      /// <summary>
+      /// Create a new <see cref="Option{T}" /> from the provided <paramref name="value" />
+      /// </summary>
+      /// <typeparam name="T">The type pf the resultign <see cref="Option{T}" /></typeparam>
+      /// <param name="value">The value to wrap in an option type</param>
       public static Option<T> Of<T>( T value )
       {
          return new Option<T>( value );
       }
 
+      /// <summary>
+      /// If <see cref="Option{T}.HasValue" /> is <c>true</c>, returns <see cref="Option{T}.Value" />, else returns
+      /// <paramref name="alternateValue" />
+      /// </summary>
+      public static T Or<T>( this Option<T> option, T alternateValue )
+      {
+         return option.HasValue ? option.Value : alternateValue;
+      }
+
+      /// <summary>
+      /// If <see cref="Option{T}.HasValue" /> is <c>true</c>, returns <see cref="Option{T}.Value" />, else resolves deferred
+      /// <paramref name="alternateValue" />
+      /// </summary>
+      public static T Or<T>( this Option<T> option, Deferred<T> alternateValue )
+      {
+         return option.HasValue ? option.Value : alternateValue.Value;
+      }
+
+      /// <summary>
+      /// If <see cref="Option{T}.HasValue" /> is <c>true</c>, returns <see cref="Option{T}.Value" />, else returns the result of
+      /// executing <paramref name="alternateValue" />
+      /// </summary>
+      public static T Or<T>( this Option<T> option, Func<T> alternateValue )
+      {
+         Contract.Requires<ArgumentNullException>( alternateValue != null );
+         return option.HasValue ? option.Value : alternateValue();
+      }
+
+      /// <summary>
+      /// If <see cref="Option{T}.HasValue" /> is <c>true</c>, returns <see cref="Option{T}.Value" />, else returns
+      /// <c>default(T)</c>
+      /// </summary>
       public static T OrDefault<T>( this Option<T> option )
       {
          return option.HasValue ? option.Value : default(T);
       }
 
+      /// <summary>
+      /// If <see cref="Option{T}.HasValue" /> is <c>true</c>, returns <see cref="Option{T}.Value" />, else returns
+      /// <c>null</c>
+      /// </summary>
       public static T OrNull<T>( this Option<T> option ) where T : class
       {
          return option.HasValue ? option.Value : null;
       }
 
+      /// <summary>
+      /// If <see cref="Option{T}.HasValue" /> is <c>true</c>, returns <see cref="Option{T}.Value" />, else returns
+      /// <c>T?</c>
+      /// </summary>
       public static T? OrNullable<T>( this Option<T> option ) where T : struct
       {
          return option.HasValue ? option.Value : (T?)null;
