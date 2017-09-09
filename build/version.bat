8fef8bea9df5fa5c46adee680829c905f9f2c577
Malachi Griffie <malachi@nexussays.com>
2017-09-09 02:51:14 -0700

    updating project files

--- build/version.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-set VERSION={"build-id":%BUILD_BUILDID%, "version":"%GITVERSION_SEMVER%", "branch":"%GITVERSION_BRANCHNAME%", "build-num":"%BUILD_BUILDNUMBER%", "user":"%BUILD_REQUESTEDFOR% <%BUILD_REQUESTEDFOREMAIL%>"}
-
-echo %VERSION%
-echo %VERSION% > version.json
--- build/version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo {\"build-id\":$BUILD_BUILDID, \"version\":\"$GITVERSION_SEMVER\", \"branch\":\"$GITVERSION_BRANCHNAME\", \"build-num\":\"$BUILD_BUILDNUMBER\", \"user\":\"$BUILD_REQUESTEDFOR \<$BUILD_REQUESTEDFOREMAIL\>\"}
-echo {\"build-id\":$BUILD_BUILDID, \"version\":\"$GITVERSION_SEMVER\", \"branch\":\"$GITVERSION_BRANCHNAME\", \"build-num\":\"$BUILD_BUILDNUMBER\", \"user\":\"$BUILD_REQUESTEDFOR \<$BUILD_REQUESTEDFOREMAIL\>\"} > version.json
